2202eb325d2b9a3fbadb7b9e0a268faed45d986f
kmc7468 <kmc7468@naver.com>
2019-02-07 16:33:45 +0900

    get_function_from_dll 함수를 사용할 수 없던 오류 수정

--- include/lml_pae/dll.hpp
+++ include/lml_pae/dll.hpp
@@ -15,13 +15,9 @@ namespace lml_pae
 
 	dll_handle open_dll(const string& path) noexcept;
 	void close_dll(dll_handle dll) noexcept;
-	template<typename Result_, typename... Args_>
-	auto get_function_from_dll(dll_handle dll, const std::string& name) noexcept
-		-> Result_(*)(Args_...);
-	template<typename Result_, typename... Args_>
-	auto get_function_from_dll<Result_(Args_...)>(dll_handle dll, const std::string& name) noexcept
-		-> Result_(*)(Args_...)
+	template<typename Ty_>
+	Ty_ get_function_from_dll(dll_handle dll, const std::string& name) noexcept
 	{
-		return reinterpret_cast<Result_(*)(Args_...)>(details::get_function_address_from_dll(dll, name));
+		return reinterpret_cast<Ty_>(details::get_function_address_from_dll(dll, name));
 	}
 }
